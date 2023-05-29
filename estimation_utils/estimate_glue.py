5cc389f71917045182798480522dac9e04f8b111
Viacheslav Meshchaninov <meshchaninov.olymp@yandex.ru>
2023-05-29 14:28:31 +0300

    sst2-estimation v1

---
+++ estimation_utils/estimate_glue.py
@@ -0,0 +1,35 @@
+import torch
+from utils.util import dict_to_cuda
+
+
+@torch.no_grad()
+def estimate_sst2(diffusion):
+    diffusion.set_valid_data_generator()
+
+    num_right = 0.
+    num = 0.
+
+    for X in diffusion.valid_loader:
+        X = dict_to_cuda(X)
+        cond_X = diffusion.sampler_emb({"input_ids": X["cond_ids"], "attention_mask": X["cond_mask"]})
+        # attention_mask = torch.zeros_like(X["cond_mask"])
+        # attention_mask[:, :3] = 1
+        pred_embeddings = diffusion.pred_embeddings(cond_X.shape[0], cond=cond_X, cond_mask=X["cond_mask"])
+        output = diffusion.pred_logits(pred_embeddings)
+
+        positive_ind = 3893
+        negative_ind = 4997
+
+        positive_proba = output[:, 1, positive_ind]
+        negative_proba = output[:, 1, negative_ind]
+
+        label = torch.where(
+            negative_proba < positive_proba,
+            positive_ind,
+            negative_ind
+        )
+        target = X["input_ids"][:, 1]
+
+        num_right += torch.sum((label == target) * 1.).item()
+        num += target.shape[0]
+    return num_right, num

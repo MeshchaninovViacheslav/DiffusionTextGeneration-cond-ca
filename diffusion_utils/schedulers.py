72c04847495740c10b8e349ee20a7810f784c250
Viacheslav <meshchaninov01@mail.ru>
2024-01-27 11:05:28 +0300

    schedulers update

--- diffusion_utils/schedulers.py
+++ diffusion_utils/schedulers.py
@@ -4,10 +4,13 @@ from abc import ABCMeta, abstractmethod
 
 
 def create_scheduler(config):
-    if config.dynamic.scheduler == "cosine":
-        return Cosine(config.dynamic.beta_min, config.dynamic.beta_max)
+    if config.dynamic.scheduler == "linear":
+        return Linear(config.dynamic.beta_min, config.dynamic.beta_max)
     elif config.dynamic.scheduler == "sd":
-        return CosineSD(config.dynamic.coef_d)
+        return SD(config.dynamic.coef_d)
+    elif config.dynamic.scheduler == "cosine":
+        return CosineIDDPM()
+
 
 
 class Scheduler(metaclass=ABCMeta):
@@ -23,7 +26,7 @@ class Scheduler(metaclass=ABCMeta):
         pass
 
 
-class Cosine(Scheduler):
+class Linear(Scheduler):
     def __init__(self, beta_0, beta_1):
         self.beta_0 = beta_0
         self.beta_1 = beta_1
@@ -40,7 +43,7 @@ class Cosine(Scheduler):
         return alpha, std
 
 
-class CosineSD(Scheduler):
+class SD(Scheduler):
     def __init__(self, d=1):
         self.d = d
         self.t_thr = 0.95
@@ -57,3 +60,25 @@ class CosineSD(Scheduler):
         alpha_t = 1 / torch.sqrt(1 + tan ** 2 * self.d ** 2)
         std_t = torch.sqrt(1 - alpha_t ** 2)
         return torch.clip(alpha_t, 0, 1), torch.clip(std_t, 0, 1)
+
+
+class CosineIDDPM(Scheduler):
+    def __init__(self):
+        self.s = 0.008
+
+    def beta_t(self, t):
+        beta_t = np.pi / (1 + self.s) / self.f(t) * torch.sin((t + self.s) / (1 + self.s) * np.pi / 2)
+        beta_t = torch.clip(beta_t, 0, 1000)
+        return beta_t
+
+    def f(self, t):
+        return torch.cos((t + self.s) / (1 + self.s) * np.pi / 2)
+
+    @property
+    def f_0(self):
+        return np.cos(self.s / (1 + self.s) * np.pi / 2)
+
+    def params(self, t):
+        mu = torch.clip(self.f(t)[:, None, None].to(t.device) / self.f_0, 0, 1)
+        std = torch.sqrt(1. - mu ** 2)
+        return torch.clip(mu, 0, 1), torch.clip(std, 0, 1)

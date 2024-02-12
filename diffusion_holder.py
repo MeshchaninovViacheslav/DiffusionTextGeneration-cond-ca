0f57a273c26bbcdf75fa2bd2953cb1717443f643
Viacheslav <meshchaninov01@mail.ru>
2024-02-12 13:42:34 +0300

    conditional decoder fix

--- diffusion_holder.py
+++ diffusion_holder.py
@@ -67,7 +67,7 @@ class DiffusionRunner:
         ).eval().cuda()
 
         # Decoder
-        self.decoder = BertDecoder(model_name=config.model.encoder_name, mode=config.model.decoder_mode)
+        self.decoder = BertDecoder(model_name=config.model.encoder_name, mode=config.model.decoder_mode, is_cond=config.model.decoder_is_cond)
         self.restore_decoder()
         self.decoder = self.decoder.cuda().eval()
 
